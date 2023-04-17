9464b6535d9039468e1ef33d8daf16da214bcf31
Patrick Mwaniki <patrickmwanikk@gmail.com>
2023-04-17 15:03:50 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Water Bill Calculator</title>
+</head>
+
+<body>
+	<div class="container">
+		<h1>Water Bill Calculator</h1>
+		<form method="POST">
+			<div class="form-group">
+				<label for="units">Enter the number of units:</label>
+				<input type="number" name="units" class="form-control" id="units" required>
+			</div>
+			<button type="submit" class="btn btn-primary">Calculate Bill</button>
+		</form>
+
+	</div>
+</body>
+
+</html>
